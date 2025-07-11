798f54988a659a875178766ad9bf1a8316dd2cbc
Mmah-Zombo <www.mariamzombo@gmail.com>
2025-07-11 15:47:00 +0000

    Fix Frontend Folder

---
+++ api-backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of products.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created product in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified product.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified product in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified product from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
